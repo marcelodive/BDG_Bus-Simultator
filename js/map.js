d44c1a33187dd8994ed7bf237e7f9de8e380838e
marcelodive <marcelo.fejemg@fejemg.org.br>
2016-06-15 23:57:21 -0300

    Forget the mistake

--- js/map.js
+++ js/map.js
@@ -1,14 +1,16 @@
 var map = L.map('map');
-
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+L.control.scale().addTo(map);
+
+var wayP = [
+    	    L.latLng(-19.877157, -43.976565),
+    	    L.latLng(-19.863042, -43.958176)	    
+    	];
 
 var routing = L.Routing.control({
-	waypoints: [
-	    L.latLng(57.6792, 11.949),
-		L.latLng(57.74, 11.94)
-	]
+	waypoints: wayP,
 }).addTo(map);
 
 $('.mapBtn').on('click', addMarkers);
@@ -16,8 +18,10 @@ $('.mapBtn').on('click', addMarkers);
 function addMarkers(){
 	console.log([routing._routes["0"]["coordinates"]]);
 	
+	console.log(routing._routes["0"].summary.totalDistance);
+	
 	var myMovingMarker = L.Marker.movingMarker(routing._routes["0"]["coordinates"],
-	        [2]).addTo(map);	
+	        10000).addTo(map);	
 	myMovingMarker.start();
 }
 
